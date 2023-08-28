359b8267e011d24c9cf3e9774f2801749ae7ff2d
Browndy <57893396+Browndy@users.noreply.github.com>
2023-08-28 16:49:13 +0200

    Update script.user.js

--- packs/classic/script/script.user.js
+++ packs/classic/script/script.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @author          Browndy
 // @name            Tribalwars Classic Map
+// @version         1.0
+// @author          Browndy
+// @description     *
 // @homepage        https://gist.github.com/Browndy/550af7a1c4d505fc118a5c1b2df95f38/raw/script.user.js
 // @namespace       *
-// @include         https://de*.die-staemme.de*screen=map*
-// @grant           GM_addStyle
-// @require         https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @match           https://*.die-staemme.de/game.php?*&screen=map*
 // @run-at          document-end
 // ==/UserScript==
 
@@ -14,18 +14,10 @@ var map = typeof unsafeWindow != 'undefined' ? unsafeWindow.TWMap : window.TWMap
 map.graphics = "https://raw.githubusercontent.com/Browndy/tw-style-packs/master/packs/classic/graphics/";
 map.graphics += map.night ? "n_" : "";
 
-$("#map")[0].style.backgroundImage = 'url(' + map.graphics + 'gras4.png)'
-
-$("img[id^='map_village']").each(function() {
-	var path = this.src.split("/");
-	var fileName = path[path.length -1];
-	this.src = map.graphics + fileName;
-});
-
 
 // document-idle
 window.addEventListener ("load", function() {
+	map.reload()
     if (game_data.player.is_guest === undefined) return;
     $("#map_container")[0].children[0].remove();
 });
-
