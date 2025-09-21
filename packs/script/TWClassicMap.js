22484b5b181832e6197a433ceab67d04b6c4692f
Browndy <57893396+Browndy@users.noreply.github.com>
2025-09-22 00:38:07 +0200

    Added reload

--- packs/script/TWClassicMap.js
+++ packs/script/TWClassicMap.js
@@ -8,6 +8,8 @@
 	
 	map.graphics = "https://raw.githubusercontent.com/Browndy/tw-style-packs/master/packs/" + pack + "/graphics/";
 	map.graphics += map.night ? "n_" : "";
+
+	map.reload();
 	
 	// document-idle
 	window.addEventListener ("load", function() {
