25c67d082b0c7a1e9a1caa74ece3b3dd2389c382
Browndy <57893396+Browndy@users.noreply.github.com>
2025-09-22 00:08:41 +0200

    Add TWClassicMap.js to packs/script

---
+++ packs/script/TWClassicMap.js
@@ -0,0 +1,17 @@
+(function () {
+	const win = typeof unsafeWindow != 'undefined' ? unsafeWindow : window;
+	const map = typeof unsafeWindow != 'undefined' ? unsafeWindow.TWMap : window.TWMap;
+	
+	// classic, new, new_skins
+	const pack = win.TWCM_pack || "new";
+	
+	map.graphics = "https://raw.githubusercontent.com/Browndy/tw-style-packs/master/packs/" + pack + "/graphics/";
+	map.graphics += map.night ? "n_" : "";
+	
+	// document-idle
+	window.addEventListener ("load", function() {
+		map.reload()
+    	if (game_data.player.is_guest === undefined) return;
+    	$("#map_container")[0].children[0].remove();
+	});
+})();
